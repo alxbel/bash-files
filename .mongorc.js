d9d72cff122a51723b8223d546f7d98ce61081ef
alexbel <lexblacken@gmail.com>
2016-09-09 17:51:46 +0300

    _

---
+++ .mongorc.js
@@ -0,0 +1,4 @@
+// mongorc.js
+var compliment = ["attractive", "intelligent", "like Batman"];
+var index = Math.floor(Math.random()*3);
+print("Hello, you're looking particularly "+compliment[index]+" today!");
