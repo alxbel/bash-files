144502355e270a2483ee778ec2c29b64ad20d491
Alex Bel <lexblacken@gmail.com>
2018-11-23 12:47:22 +0300

    Delete .mongorc.js

--- .mongorc.js
+++
@@ -1,4 +0,0 @@
-// mongorc.js
-var compliment = ["attractive", "intelligent", "like Batman"];
-var index = Math.floor(Math.random()*3);
-print("Hello, you're looking particularly "+compliment[index]+" today!");
